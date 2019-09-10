69d64fdaea063e0eb3bf14f04a5842fbce31e9fe
Sam Callahan <callahan.l.sam@gmail.com>
2019-09-10 17:05:47 -0500

    Started working on list comprhensions

---
+++ 17_list_comprehensions.py
@@ -0,0 +1,89 @@
+# 17 list comprehension problems in python
+
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 17, 19, 23, 256, -8, -4, -2, 5, -9]
+
+# Example for loop solution to add 1 to each number in the list
+numbers_plus_one = []
+for number in numbers:
+    numbers_plus_one.append(number + 1)
+
+# Example of using a list comprehension to create a list of the numbers plus one.
+numbers_plus_one = [number + 1 for number in numbers]
+
+# Example code that creates a list of all of the list of strings in fruits and uppercases every string
+output = []
+for fruit in fruits:
+    output.append(fruit.upper())
+    
+# Exercise 1 - rewrite the above example code using list comprehension syntax. Make a variable named uppercased_fruits to hold the output of the list comprehension. Output should be ['MANGO', 'KIWI', etc...]
+uppercased_fruits = [fruit.upper() for fruit in fruits]
+
+# Exercise 2 - create a variable named capitalized_fruits and use list comprehension syntax to produce output like ['Mango', 'Kiwi', 'Strawberry', etc...]
+capitalized_fruits = [fruit.capitalize() for fruit in fruits]
+
+# Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
+def is_vowel(character):
+    vowels = "AaEeIiOoUu"
+    if character in vowels:
+        return True
+    else:
+        return False
+
+def vowel_count(word):
+    count = 0
+    for character in word:
+        if is_vowel(character):
+            count += 1
+    return count
+
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if vowel_count(fruit) > 2]
+
+# Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
+fruits_with_only_two_vowels = [fruit for fruit in fruits if vowel_count(fruit) == 2]
+
+# Exercise 5 - make a list that contains each fruit with more than 5 characters
+fruits_with_more_than_five_characters = [fruit in fruits if len(fruit) > 5]
+
+# Exercise 6 - make a list that contains each fruit with exactly 5 characters
+fruits_with_five_characters = [fruit in fruits if len(fruit) == 5]
+
+# Exercise 7 - Make a list that contains fruits that have less than 5 characters
+fruits_with_less_than_five_characters = [fruit in fruits if len(fruit) < 5]
+
+# Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
+characters_in_fruit_name = [len(fruit) for fruit in fruits]
+
+# Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
+fruits_with_letter_a = [fruit for fruit in fruits if "a" in fruit]
+
+# Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
+even_numbers = [number for number in numbers if number % 2 == 0]
+
+# Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+
+# Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
+
+# Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+
+# Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
+
+# Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
+
+# Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
+
+# Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing each number plus five. 
+
+# BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers
+def is_prime(number):
+    number = int(number ** 2 / number)
+    possible_factors = range(1,number // 2)
+    prime = True
+    for factor in possible_factors:
+        if number % factor == 0:
+            prime = False
+    return prime
+
+primes = [number for number in numbers if is_prime(number)]
+primes

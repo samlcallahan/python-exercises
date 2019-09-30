b0a16869d90f701dad0def273319d69981ce014c
Sam Callahan <callahan.l.sam@gmail.com>
2019-09-30 09:37:55 -0500

    Solved NPR puzzle programmatically

---
+++ npr.py
@@ -0,0 +1,19 @@
+with open('/usr/share/dict/words') as f:
+    words = f.read().strip().split('\n')
+
+import pandas as pd
+
+def follows_rule(word):
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    ought_sum = letters.find(word[0].lower()) + 1
+    actual_sum = 0
+    for letter in word[1:]:
+        actual_sum += letters.find(letter.lower()) + 1
+    return ought_sum == actual_sum
+
+results = []
+for i in words:
+    results.append(follows_rule(i))
+
+words = pd.Series(words)
+print(words[results])
